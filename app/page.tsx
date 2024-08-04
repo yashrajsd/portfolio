b38cbfb7f497461b4ca30a2db0abc0e6074fc67c
Yashraj Deshmukh <yashrajdeshmukh@eng.rizvi.edu.in>
2024-08-05 00:53:14 +0530

    error with page tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,6 @@ export default function Home() {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smooth: true,
     });
 
     function raf(time) {
