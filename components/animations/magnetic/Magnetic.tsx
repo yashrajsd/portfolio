c5b5276fca6b2a0fe0a0136e72a30ad0f3c51531
Yashraj Deshmukh <yashrajdeshmukh@eng.rizvi.edu.in>
2024-08-05 00:59:42 +0530

    gg

--- components/animations/magnetic/Magnetic.tsx
+++ components/animations/magnetic/Magnetic.tsx
@@ -1,13 +1,13 @@
 'use client'
 import { motion } from 'framer-motion'
-import { useRef, useState } from 'react'
+import { ReactNode, useRef, useState } from 'react'
 
-export default function Magnetic({ children }) {
+export default function Magnetic({ children }:any) {
     const ref = useRef(null)
     const [position, setPosition] = useState({ x: 0, y: 0 })
     const maxDisplacement = 20
 
-    const mouseMove = (e) => {
+    const mouseMove = (e:any) => {
         const { clientX, clientY } = e
         const { width, height, left, top } = ref.current.getBoundingClientRect()
         let x = clientX - (left + width / 2)
