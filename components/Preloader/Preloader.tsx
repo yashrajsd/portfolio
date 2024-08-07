3e41401e83b1051a9a67bde23bac7dc14fd8f7ec
Yashraj Deshmukh <yashrajdeshmukh@eng.rizvi.edu.in>
2024-08-07 23:30:10 +0530

    preloader issue resolver

--- components/Preloader/Preloader.tsx
+++ components/Preloader/Preloader.tsx
@@ -23,12 +23,14 @@ export default function Preloader(){
         "नमस्कार"
     ];
 
-    useEffect(()=>{
-        if(index==words.length-1)return;
-        setTimeout(()=>{
-            setIndex(index+1);
-        },index==0?1000:150)
-    },[])
+    useEffect(() => {
+        if (index === words.length - 1) return;
+        const timeout = setTimeout(() => {
+            setIndex(index + 1);
+        }, index === 0 ? 1000 : 150);
+
+        return () => clearTimeout(timeout);
+    }, [index])
 
     return(
         <motion.div 
@@ -36,12 +38,12 @@ export default function Preloader(){
         exit={{y:'-100vh'}}
         transition={{duration:1.3,ease:[0.76,0,0.24,1]}}
         className="fixed top-0 left-0 w-[100vw] h-[100vh] z-[9999] bg-white flex items-center justify-center">
-            <div className='relative flex justify-center items-center z-[10] w-full h-full'>
-            <div className='absolute top-0 h-[10vh] border-b-[#5B5B5B] border-[1px] w-full'/>
-            <div className='absolute right-0 h-full border-l-[#5B5B5B] w-[10vh] border-[1px]'/>
-            <div className='absolute bottom-0 h-[10vh]  border-r-[#5B5B5B] w-full border-[1px]'/>
-            <div className='absolute left-0 h-full  border-t-[#5B5B5B] w-[10vh] border-[1px]'/>
-            <div className='border-t-[#5B5B5B] w-[80vh] flex justify-center items-center h-[80vh] rounded-full border-[1px]'>
+            <div className='relative flex justify-center items-center z-[10] w-full border-solid h-full'>
+            <div className='absolute top-0 h-[10vh] border-b-[#5B5B5B] border-[1px] border-solid w-full'/>
+            <div className='absolute right-0 h-full border-l-[#5B5B5B] w-[10vh] border-solid border-[1px]'/>
+            <div className='absolute bottom-0 h-[10vh]  border-r-[#5B5B5B] w-full border-solid border-[1px]'/>
+            <div className='absolute left-0 h-full  border-t-[#5B5B5B] w-[10vh] border-solid border-[1px]'/>
+            <div className='border-t-[#5B5B5B] w-[80vh] flex justify-center items-center border-solid h-[80vh] rounded-full border-[1px]'>
             <p className={`${Hanken600.className} text-[2vw] text-[#252525] z-[1000]`}>{words[index]}</p>
             </div>
             </div>
